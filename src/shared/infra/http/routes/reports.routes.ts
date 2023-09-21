92edf1225443fbcf484ef25c395678bfad712294
Lucas Corrêa <lucascorrea@MacBook-Air-de-Lucas.local>
2023-09-21 14:27:53 -0300

    Correção de bug no import

--- src/shared/infra/http/routes/reports.routes.ts
+++ src/shared/infra/http/routes/reports.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { NPSSurveyAnswersController } from "../../../../reports/NPSSurveyAnswers/useCase/NPSSurveyAnswersController";
 import { ScheduleController } from "../../../../reports/Schedules/useCase/SchedulesController";
-import { ResponsesReportController } from "reports/ResponsesReport/useCase/ResponsesReportController";
+import { ResponsesReportController } from "../../../../reports/ResponsesReport/useCase/ResponsesReportController";
 
 const reportsRoutes = Router();
 
