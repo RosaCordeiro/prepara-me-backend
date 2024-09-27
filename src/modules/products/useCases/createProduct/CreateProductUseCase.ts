81fb800e59ff15cd55f4ea556f2f26dd744e9c3b
Lucas Corrêa <lucascorrea@MacBook-Air-de-Lucas.local>
2024-09-27 12:11:17 -0300

    COrreção edição de produto

--- src/modules/products/useCases/createProduct/CreateProductUseCase.ts
+++ src/modules/products/useCases/createProduct/CreateProductUseCase.ts
@@ -52,7 +52,7 @@ class CreateProductUseCase {
 
         const response = await this.productsRepository.findBySlug(slug);
 
-        if (response) {
+        if (!id && response) {
             throw new AppError("Slug already exists");
         }
 
