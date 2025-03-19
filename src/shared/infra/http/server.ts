ab8766be9a1596ad839997c694f60981d0f9a31e
Lucas Campanuchi <lucas.correa@okn.com.br>
2025-03-19 17:32:21 +0000

    Update server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,8 @@ import { app } from "./app";
 import { config } from 'dotenv'
 config()
 
-app.listen(parseInt(process.env.PORT ?? '3334'), () => console.log("Server is running on 3334!"));
+const port = parseInt(process.env.PORT ?? '3334');
+
+app.listen(port, () => console.log(`Server is running on ${port}!`));
 
 
